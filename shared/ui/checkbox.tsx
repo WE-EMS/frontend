afbe7680e6884a02b81dcce08f87645140ae9204
mxkxx <mxkxx@naver.com>
2025-08-03 01:34:25 +0900

    [#14] feat: checkbox 컴포넌트 사이즈 Prop 추가

--- shared/ui/checkbox.tsx
+++ shared/ui/checkbox.tsx
@@ -5,39 +5,39 @@ import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { cn } from '@/shared/util/index';
 import { IconCheck } from '../asset/icon';
 import { ComponentProps } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
 
-function Checkbox({
-  className,
-  ...props
-}: ComponentProps<typeof CheckboxPrimitive.Root>) {
+const baseCheckboxStyle = `
+  cursor-pointer 
+  data-[state=checked]:bg-main-green-700 data-[state=checked]:border-main-green-700
+  focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]
+  aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive
+  size-4 shrink-0 rounded-[4px] border border-main-green-700
+  transition-shadow outline-none
+  disabled:cursor-not-allowed disabled:opacity-50
+`;
+
+const checkboxVariants = cva(baseCheckboxStyle, {
+  variants: {
+    size: {
+      sm: 'size-4',
+      md: 'size-5', // 기본값
+      lg: 'size-6',
+    },
+  },
+  defaultVariants: {
+    size: 'sm',
+  },
+});
+
+type CheckboxProps = ComponentProps<typeof CheckboxPrimitive.Root> &
+  VariantProps<typeof checkboxVariants>;
+
+function Checkbox({ className, size, ...props }: CheckboxProps) {
   return (
     <CheckboxPrimitive.Root
       data-slot='checkbox'
-      className={cn(
-        'cursor-pointer',
-
-        // 상태 기반 스타일
-        'data-[state=checked]:bg-main-green-700',
-        'data-[state=checked]:border-main-green-700',
-
-        // focus 상태 (접근성)
-        'focus-visible:border-ring',
-        'focus-visible:ring-ring/50',
-        'focus-visible:ring-[3px]',
-
-        // 에러 상태
-        'aria-invalid:ring-destructive/20',
-        'dark:aria-invalid:ring-destructive/40',
-        'aria-invalid:border-destructive',
-
-        // 레이아웃 및 스타일
-        'size-4 shrink-0 rounded-[4px] border border-main-green-700',
-        'transition-shadow outline-none',
-
-        // 비활성 상태
-        'disabled:cursor-not-allowed disabled:opacity-50',
-        className,
-      )}
+      className={cn(checkboxVariants({ size }), className)}
       {...props}
     >
       <CheckboxPrimitive.Indicator
