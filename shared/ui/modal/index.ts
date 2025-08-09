609ce8e516f030b408f0e6abd63cf1375973f4af
mxkxx <mxkxx@naver.com>
2025-08-09 19:54:49 +0900

    feat: 모달 내부 섹션 하위컴포넌트 추가

--- shared/ui/modal/index.ts
+++ shared/ui/modal/index.ts
@@ -5,5 +5,7 @@ export { ModalCloseWrapper } from './modal-close-wrapper';
 export { ModalPortal } from './modal-portal';
 export { ModalButtons } from './modal-buttons';
 export { ModalDescription } from './modal-description';
+export { ModalSection } from './modal-section';
+export { ModalSectionTitle } from './modal-section-title';
 
 export { useModalStore } from './use-modal-store';
